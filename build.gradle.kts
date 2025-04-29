9366fbdab800bb3ee9191bedd07068a165c9094b
Kostas Zoumpatianos <kostas.zoumpatianos@fivetran.com>
2025-04-29 19:10:22 +0200

    gradle kts fix

--- build.gradle.kts
+++ build.gradle.kts
@@ -124,7 +124,6 @@ tasks.named<RatTask>("rat").configure {
   excludes.add("**/go.sum")
 
   excludes.add("**/kotlin-compiler*")
-
   excludes.add("**/build-logic/.kotlin/**")
 
   excludes.add("plugins/**/*.ref")
